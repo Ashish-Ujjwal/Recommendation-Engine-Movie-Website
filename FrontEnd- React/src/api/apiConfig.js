b4b7988c309cb9bd3c2ad9843a89d888d16dce13
Ashish Ujjwal <ashishfrommohania@gamil.com>
2024-05-28 14:57:56 +0530

    some updates

--- FrontEnd- React/src/api/apiConfig.js
+++ FrontEnd- React/src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    baseUrl_RecEng: 'https://recommendation-engine-assim.herokuapp.com//',
+    baseUrl_RecEng: 'https://recommendation-engine-ashish.herokuapp.com//',
     apiKey: 'f2ea12b22e7afbfd7bf179d7bcbe392b',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
